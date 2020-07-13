f4dbe11d8971da2f3385c8591c9f771a35ed0a4c
mrbudbud <rachmad.16421044@student.ubl.ac.id>
2020-07-13 20:40:04 +0700

    register

--- register.php
+++ register.php
@@ -1,39 +1,35 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-
-    <title>Halaman Registrasi</tittle>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
+    <h1>oke</h1>
 
-    <h1>Halaman Registrasi</h1>
+      <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">username :</label>
+                <input type="text" name="username" id="username">
+            </li>
+            
+            <li>
+                <label for="password">password :</label>
+                <input type="password" name="password" id="password">
+            </li>
 
-<form action="" method="post">
-
-<ul>
-
-    <li>
-
-        <label for="username">username :</label>
-        <input type="text" name="username" id="username">
-
-    </li>
-    <li>
-        <label for="password">password :</label>
-        <input type="password" name="password" id="password">
-
-    </li>
-    <li>
-        <label for="password2">konfirmasi password :</label>
-        <input type="password" name="password2" id="">
-    </li>
-    <li>
-        <button type="submit" name="daftar">Daftar</button>
-    </li>
-</ul>
-
-</form>
-
+            <li>
+                <label for="password2">konfirmasi password :</label>
+                <input type="password" name="password2" id="">
+            </li>
+            <li>
+                <button type="submit" name="daftar">Daftar</button>
+            </li>
+        </ul>
+    </form>
 </body>
 </html>
+
+          
