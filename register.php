c5fc77f7b6390ad7502d0dc703146299bba63e54
OktaviaLaurina <oktavia.17421032@student.ubl.ac.id>
2020-07-13 20:31:59 +0700

    first commit

---
+++ register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Halaman Registrasi</tittle>
+
+</head>
+<body>
+
+    <h1>Halaman Registrasi</h1>
+
+<form action="" method="post">
+
+<ul>
+
+    <li>
+
+        <label for="username">username :</label>
+        <input type="text" name="username" id="username">
+
+    </li>
+    <li>
+        <label for="password">password :</label>
+        <input type="password" name="password" id="password">
+
+    </li>
+    <li>
+        <label for="password2">konfirmasi password :</label>
+        <input type="password" name="password2" id="">
+    </li>
+    <li>
+        <button type="submit" name="daftar">Daftar</button>
+    </li>
+</ul>
+
+</form>
+
+</body>
+</html>
